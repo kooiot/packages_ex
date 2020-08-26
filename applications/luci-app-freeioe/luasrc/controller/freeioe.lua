005dc5f69fb33a0627b4ffb657752bb1c89afb99
Dirk Chang <dirk@kooiot.com>
2020-08-26 15:57:06 +0800

    acl depends for menu

--- applications/luci-app-freeioe/luasrc/controller/freeioe.lua
+++ applications/luci-app-freeioe/luasrc/controller/freeioe.lua
@@ -1,4 +1,4 @@
--- Copyright 2016-2017 Dan Luedtke <mail@danrl.com>
+-- Copyright 2020 Dirk Chang <dirk@kooiot.com>
 -- Licensed to the public under the Apache License 2.0.
 
 module("luci.controller.freeioe", package.seeall)
@@ -8,16 +8,16 @@ function index()
 		return
 	end
 
-	entry({"admin", "freeioe"}, template("freeioe/jump"), _("FreeIOE"))
-	--[[
+	--entry({"admin", "freeioe"}, template("freeioe/jump"), _("FreeIOE"))
 	local e = entry({"admin", "freeioe"}, firstchild(), _("FreeIOE"), 50)
 	e.dependent = false
-	]]
+	e.acl_depends = { "luci-freeioe-app" }
+
+	entry({"admin", "freeioe", "settings"}, template("freeioe/jump"), _("Settings"), 1).leaf = true
 
 	--[[
 	local s = entry({"admin", "freeioe", "settings"}, view("freeioe/settings"), _("Settings"), 1)
 	s.leaf = true
 	s.acl_depends = { "luci-freeioe-app" }
-	entry({"admin", "freeioe", "settings"}, view("freeioe/settings"), _("Settings"), 1).leaf = true
 	]]--
 end
