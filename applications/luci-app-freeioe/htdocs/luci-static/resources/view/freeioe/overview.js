6b407a4de609fda8d26195ffe3c31c2d368afd43
Dirk Chang <dirk@kooiot.com>
2020-08-26 19:33:00 +0800

    update views

--- applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/overview.js
+++ applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/overview.js
@@ -3,28 +3,54 @@
 'require ui';
 'require uci';
 'require rpc';
+'require view';
 'require form';
 
+var callFreeioeInfo = rpc.declare({
+	object: 'freeioe',
+	method: 'info'
+});
+
+var callFreeioeCloud = rpc.declare({
+	object: 'freeioe',
+	method: 'cloud'
+});
+
+
+
 return view.extend({
-	callFreeioeInfo = rpc.declare({
-		object: 'freeioe',
-		method: 'info',
-		expect: { result: [] }
-	});
-
-	callFreeioeCloud = rpc.declare({
-		object: 'freeioe',
-		method: 'cloud',
-		expect: { result: [] }
-	});
-
 	load: function() {
 		return Promise.all([
-			this.callFreeioeInfo(),
-			this.callFreeioeCloud(),
+			L.resolveDefault(callFreeioeInfo(), {}),
+			L.resolveDefault(callFreeioeCloud(), {})
 		]);
 	},
 
 	render: function(data) {
-	}
+		var info   = data[0],
+		    cloud  = data[1];
+
+		var fields = [
+			_('PSN'),              info.hw_id ? info.hw_id + ' / ' + info.id : null,
+			_('Version'),          info.version ? info.version + ' ( ' + info.skynet_version + ' ) ' : null,
+			_('Firmware Version'), info.firmware_version,
+			_('Cloud Host'),       cloud.host,
+			_('Cloud Status'),     cloud && cloud.mqtt ? cloud.mqtt.online === 1 ? 'ONLINE' : 'OFFLINE: ' + cloud.mqtt.msg : null,
+		];
+
+		var table = E('div', { 'class': 'table' });
+
+		for (var i = 0; i < fields.length; i += 2) {
+			table.appendChild(E('div', { 'class': 'tr' }, [
+				E('div', { 'class': 'td left', 'width': '33%' }, [ fields[i] ]),
+				E('div', { 'class': 'td left' }, [ (fields[i + 1] != null) ? fields[i + 1] : '?' ])
+			]));
+		}
+
+		return table
+	},
+
+	handleSaveApply: null,
+	handleSave: null,
+	handleReset: null
 });
--- applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/settings.js
+++ applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/settings.js
@@ -3,28 +3,78 @@
 'require ui';
 'require uci';
 'require rpc';
+'require view';
 'require form';
 
+var callFreeioeCloudOption = rpc.declare({
+	object: 'freeioe',
+	method: 'cloud'
+});
+
 return view.extend({
-	callFreeioeInfo = rpc.declare({
-		object: 'freeioe',
-		method: 'info',
-		expect: { result: [] }
-	});
-
-	callFreeioeCloud = rpc.declare({
-		object: 'freeioe',
-		method: 'cloud',
-		expect: { result: [] }
-	});
-
 	load: function() {
 		return Promise.all([
-			this.callFreeioeInfo(),
-			this.callFreeioeCloud(),
+			L.resolveDefault(callFreeioeCloudOption(), {})
 		]);
 	},
 
 	render: function(data) {
-	}
+		var cloud   = data[0],
+			m, s, o;
+
+		m = new form.Map('freeioe',
+			_('FreeIOE'),
+			_('<a href="http://freeioe.org">FreeIOE</a> is an opensource IOT Computing-Edge Platform.'));
+
+		s = m.section(form.TypedSection, 'freeioe', _('Settings'));
+		s.anonymous = true;
+		s.addremove = true;
+
+		o = s.option(form.Value, 'host', _('Host'));
+		o.datatype    = 'host';
+		o.placeholder = 'ioe.thingsroot.com';
+
+		o = s.option(form.Value, 'port', _('Port'));
+		o.datatype    = 'port';
+		o.placeholder = 22;
+
+		o = s.option(form.Value, 'pkg_host', _('App Center'));
+		o.datatype    = 'host';
+		o.placeholder = 'ioe.thingsroot.com';
+
+		o = s.option(form.Value, 'cnf_host', _('Conf Center'));
+		o.datatype    = 'host';
+		o.placeholder = 'ioe.thingsroot.com';
+
+		return m.render();
+	},
+
+	handleSave: function() {
+		var map = document.querySelector('.cbi-map');
+
+		return dom.callClassMethod(map, 'save').then(function() {
+			if (formData.password.pw1 == null || formData.password.pw1.length == 0)
+				return;
+
+			if (formData.password.pw1 != formData.password.pw2) {
+				ui.addNotification(null, E('p', _('Given password confirmation did not match, password not changed!')), 'danger');
+				return;
+			}
+
+			return callSetPassword("root", formData.password.pw1).then(function(success) {
+				if (success)
+					ui.addNotification(null, E('p', _('The system password has been successfully changed.')), 'info');
+				else
+					ui.addNotification(null, E('p', _('Failed to change the system password.')), 'danger');
+
+				formData.password.pw1 = null;
+				formData.password.pw2 = null;
+
+				dom.callClassMethod(map, 'render');
+			});
+		});
+	},
+
+	handleSaveApply: null,
+	handleReset: null
 });
