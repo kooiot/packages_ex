2a0c4aa34f4560385a4e8463b48501975304ecf0
Dirk Chang <dirk@kooiot.com>
2020-08-26 16:21:54 +0800

    improve the freeioe app

--- applications/luci-app-freeioe/luasrc/controller/freeioe.lua
+++ applications/luci-app-freeioe/luasrc/controller/freeioe.lua
@@ -4,20 +4,7 @@
 module("luci.controller.freeioe", package.seeall)
 
 function index()
-	if not nixio.fs.access("/usr/ioe/skynet/cfg.json") then
-		return
-	end
-
-	--entry({"admin", "freeioe"}, template("freeioe/jump"), _("FreeIOE"))
-	local e = entry({"admin", "freeioe"}, firstchild(), _("FreeIOE"), 50)
-	e.dependent = false
-	e.acl_depends = { "luci-freeioe-app" }
-
-	entry({"admin", "freeioe", "settings"}, template("freeioe/jump"), _("Settings"), 1).leaf = true
-
-	--[[
-	local s = entry({"admin", "freeioe", "settings"}, view("freeioe/settings"), _("Settings"), 1)
-	s.leaf = true
-	s.acl_depends = { "luci-freeioe-app" }
-	]]--
+	entry({"admin", "freeioe"}, firstchild(), _("FreeIOE"), 50)
+	entry({"admin", "freeioe", "settings"}, view("freeioe/overview"), _("Overview"), 10)
+	entry({"admin", "freeioe", "settings"}, view("freeioe/settings"), _("Settings"), 20)
 end
