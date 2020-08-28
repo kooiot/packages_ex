35bf7ae75752426017a85a04cbf9a72567e5c9bb
Dirk Chang <dirk@kooiot.com>
2020-08-28 10:35:04 +0800

    update freeioe version

--- applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/settings.js
+++ applications/luci-app-freeioe/htdocs/luci-static/resources/view/freeioe/settings.js
@@ -19,6 +19,33 @@ var	callFreeioeCfgSet = rpc.declare({
 	params: [ "cfg", "conf" ]
 });
 
+var callInitAction = rpc.declare({
+	object: 'luci',
+	method: 'setInitAction',
+	params: [ 'name', 'action' ],
+	expect: { result: false }
+});
+
+var CBIFreeIOECtrl = form.DummyValue.extend({
+	renderWidget: function(section_id, option_id, cfgvalue) {
+		return E([], [
+			E('span', { 'class': 'control-group' }, [
+				E('button', {
+					'class': 'cbi-button cbi-button-save',
+					'click': ui.createHandlerFn(this, 'save')
+				}, _('Save Settings')),
+				E('span', {}, [
+				'     ',
+				]),
+				E('button', {
+					'class': 'cbi-button cbi-button-negative important',
+					'click': ui.createHandlerFn(this, 'restart')
+				}, _('Restart FreeIOE'))
+			])
+		]);
+	},
+});
+
 return view.extend({
 	load: function() {
 		return Promise.all([
@@ -32,6 +59,7 @@ return view.extend({
 
 		formData['cloud'] = data[0];
 		formData['sys'] = data[1];
+		formData['action'] = {};
 
 		m = new form.JSONMap(formData, 
 			_('Settings'),
@@ -71,10 +99,41 @@ return view.extend({
 		o.datatype    = 'cnf_host';
 		o.placeholder = 'ioe.thingsroot.com';
 
+		s = m.section(form.NamedSection, 'action', _('Actions'));
+
+		o = s.option(CBIFreeIOECtrl, '_options');
+		var view_obj = this;
+		o.save = ui.createHandlerFn(this, 'handleSaveCfg');
+		o.restart = ui.createHandlerFn(this, 'handleRestart');
+
 		return m.render();
 	},
 
-	handleSave: function() {
+	handleAction: function(name, action) {
+		return callInitAction(name, action).then(function(success) {
+			if (success != true)
+				throw _('Command failed');
+
+			return true;
+		}).catch(function(e) {
+			ui.addNotification(null, E('p', _('Failed to execute "/etc/init.d/%s %s" action: %s').format(name, action, e)));
+		});
+	},
+
+	handleRestart: function() {
+		if (!confirm(_('Do you really want to restart FreeIOE?')))
+			return;
+
+		ui.showModal(_('Rebooting...'), [
+			E('p', { 'class': 'spinning' }, _('FreeIOE will be restarted now!'))
+		]);
+		this.handleAction('skynet', 'restart');
+		window.setTimeout(L.bind(function() {
+			window.location.reload();
+		}, this), 3000);
+	},
+
+	handleSaveCfg: function() {
 		var map = document.querySelector('.cbi-map');
 
 		return dom.callClassMethod(map, 'save').then(function() {
@@ -115,6 +174,7 @@ return view.extend({
 		});
 	},
 
+	handleSave: null,
 	handleSaveApply: null,
 	handleReset: null
 });
