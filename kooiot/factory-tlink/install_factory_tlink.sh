4f91a1687d07a405fa7ff8235d6387faace24eb1
Dirk Chang <dirk@kooiot.com>
2022-01-20 18:12:36 +0800

    improve opkg key file adding

--- kooiot/factory-tlink/install_factory_tlink.sh
+++ kooiot/factory-tlink/install_factory_tlink.sh
@@ -7,8 +7,12 @@ sed -ie '/kooiot/d' /etc/opkg/customfeeds.conf
 echo "src/gz kooiot http://freeioe.oss-cn-beijing.aliyuncs.com/openwrt/${DISTRIB_RELEASE}/packages/${DISTRIB_ARCH}/kooiot" >> /etc/opkg/distfeeds.conf
 
 # sed -ie '/option check_signature/d' /etc/opkg.conf
+if [ -f /etc/opkg/keys/cce9dfbc10bada5a ]; then
+echo "opkg key exists"
+else
 echo "untrusted comment: Local build key" > /etc/opkg/keys/cce9dfbc10bada5a
 echo "RWTM6d+8ELraWs5mgLVvXjWu0abxvMzBra4vQGSnNzCBbT6+lnAcAggc" >> /etc/opkg/keys/cce9dfbc10bada5a
+fi
 
 if [ -e /etc/init.d/symlink ]; then
 	/etc/init.d/symlink stop
