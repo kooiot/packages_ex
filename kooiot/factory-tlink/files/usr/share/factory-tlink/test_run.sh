e55a233331e9b3faf271f40608714048d8f2b8a8
Dirk Chang <dirk@kooiot.com>
2022-01-21 16:29:59 +0800

    improved test run script

--- kooiot/factory-tlink/files/usr/share/factory-tlink/test_run.sh
+++ kooiot/factory-tlink/files/usr/share/factory-tlink/test_run.sh
@@ -1,12 +1,23 @@
 #! /bin/sh
 
+. /lib/functions.sh
+
+if [ -f /lib/functions/tlink.sh ]; then
+	. /lib/functions/tlink.sh
+fi
+
 if [ -f /mnt/data/factory-tlink/robot-client ]; then
-# Stop services
-	/etc/init.d/skynet stop
-	/etc/init.d/symlink stop
-# Start robot-client
+	echo "Stop services.... "
+	if [ -e /etc/init.d/skynet ]; then
+		/etc/init.d/skynet stop
+	fi
+	if [ -e /etc/init.d/symlink ]; then
+		/etc/init.d/symlink stop
+	fi
+
 	cd /mnt/data/factory-tlink
-	./robot-client
+	echo "Start factory Test $(product_sn)"
+	./robot-client --client_id="$(product_sn)"
 else
 	echo "ERROR: robot-client missing"
 fi
