03c9530aba7c8e7411528d2caf02b3a527e7886d
Dirk Chang <dirk@kooiot.com>
2023-11-02 12:07:34 +0800

    factory-tlink: 19.07 has no snapshot release now

--- kooiot/factory-tlink/install_factory_tlink.sh
+++ kooiot/factory-tlink/install_factory_tlink.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sed -i 's/openwrt.freeioe.org:81/downloads.openwrt.org/g' /etc/opkg/distfeeds.conf
+sed -i 's/19.07-SNAPSHOT/19.07.10/g' /etc/opkg/distfeeds.conf
 sed -ie '/video/d' /etc/opkg/distfeeds.conf
 sed -ie '/kooiot/d' /etc/opkg/distfeeds.conf
 sed -ie '/kooiot/d' /etc/opkg/customfeeds.conf
